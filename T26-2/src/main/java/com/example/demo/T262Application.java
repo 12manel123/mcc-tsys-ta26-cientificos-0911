3dd3a0b91fdbbfe593a3d9c03ffcb56541999eb0
Manel Castellví Cerezuela <manelcastellvicerezuela@gmail.com>
2023-11-09 18:29:23 +0100

    Add files via upload

---
+++ T26-2/src/main/java/com/example/demo/T262Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T262Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T262Application.class, args);
+	}
+
+}
